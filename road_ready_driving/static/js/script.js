a58437154848bd55c8eb158e0fbaad3f0a5b4f78
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-02-09 22:58:07 +0000

    Refactor styles to use 'Rubik' font, enhance button and heading styles, and update Font Awesome version

--- road_ready_driving/static/js/script.js
+++ road_ready_driving/static/js/script.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var dropdowns = document.querySelectorAll('.dropdown-toggle');
-    dropdowns.forEach(function(dropdown) {
-        new bootstrap.Dropdown(dropdown);
-    });
-});
+
