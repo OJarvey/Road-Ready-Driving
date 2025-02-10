875f4fb8d8cb6ef108b63f2d20d88f5933de33af
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-02-10 21:29:27 +0000

    Add about and contact pages with corresponding URLs and update navbar links

--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'includes/about.html')
+
+def about(request):
+    return render(request, 'includes/contact.html')
