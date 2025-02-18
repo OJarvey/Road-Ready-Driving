9a702e86d0bdafa8c4cc9c8e206a50638d598fe1
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-02-18 21:56:45 +0000

    Add functionality to add items to the shopping bag with quantity and redirect support

--- bag/urls.py
+++ bag/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import add_to_bag
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
