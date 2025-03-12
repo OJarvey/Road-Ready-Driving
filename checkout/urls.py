d694bc42a1acfc0281c606fbf8ee2f54a4724373
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-03-12 16:20:39 +0000

    Add cache checkout data URL and log payment intent in webhook handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.checkout, name="checkout"),
     path("save-order/", views.save_order, name="save_order"),
     path("success/<order_number>/", views.checkout_success, name="checkout_success"),
+    path("cache-checkout-data/", views.cache_checkout_data, name="cache_checkout_data"),
     path('wh/', webhook, name='webhook'),
 ]
