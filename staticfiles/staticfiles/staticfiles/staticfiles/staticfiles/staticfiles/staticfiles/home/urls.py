45fd236a687263a666e041fcf6a2dc1e6c236ecd
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-03-19 14:09:31 +0000

    Added whitenoise, update requirements.txt, update settings.py to work with whitenoise

--- staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import about
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', about, name='about'),
-    path('contact/', about, name='contact'),
-]
