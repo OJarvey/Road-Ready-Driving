78055df86fddb8a6f7d02f2dd75f9af6826c31b0
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-03-23 17:52:51 +0000

    Fixed mobile search functionality and and its icon display in header.

---
+++ static/js/base.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileSearchButton = document.getElementById('mobileSearchButton');
+    const mobileSearchForm = document.getElementById('mobileSearchForm');
+
+    mobileSearchButton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        if (mobileSearchForm.style.display === 'none' || mobileSearchForm.style.display === '') {
+            mobileSearchForm.style.display = 'block';
+        } else {
+            mobileSearchForm.style.display = 'none';
+        }
+    });
+
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    navbarToggler.addEventListener('click', function() {
+        mobileSearchForm.style.display = 'none';
+    });
+
+    document.addEventListener('click', function(event) {
+        const isClickInsideForm = mobileSearchForm.contains(event.target);
+        const isClickOnButton = mobileSearchButton.contains(event.target);
+
+        if (!isClickInsideForm && !isClickOnButton && mobileSearchForm.style.display === 'block') {
+            mobileSearchForm.style.display = 'none';
+        }
+    });
+});
